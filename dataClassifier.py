a86985c8b3cc0769e5fd7bab736d152ec170d725
Katie Sidebotham <katiesidebotham54@gmail.com>
2023-05-01 10:02:12 -0400

    working mira

--- dataClassifier.py
+++ dataClassifier.py
@@ -6,6 +6,7 @@ import perceptron
 import samples
 import sys
 import util
+import mira
 import knn
 
 TEST_SET_SIZE = 100
@@ -127,7 +128,7 @@ def readCommand(argv):
     parser = OptionParser(USAGE_STRING)
 
     parser.add_option('-c', '--classifier', help=default('The type of classifier'), choices=[
-                      'mostFrequent', 'nb', 'naiveBayes', 'perceptron', 'knn'], default='mostFrequent')
+                      'mostFrequent', 'nb', 'naiveBayes', 'perceptron', 'mira'], default='mostFrequent')
     parser.add_option('-d', '--data', help=default('Dataset to use'),
                       choices=['digits', 'faces'], default='digits')
     parser.add_option(
@@ -197,8 +198,8 @@ def readCommand(argv):
     elif (options.classifier == "perceptron"):
         classifier = perceptron.PerceptronClassifier(
             legalLabels, options.iterations)
-    elif (options.classifier == "knn"):
-        classifier = knn.knnClassifier(legalLabels, options.iterations)
+    elif (options.classifier == "mira"):
+        classifier = mira.MiraClassifier(legalLabels, options.iterations)
     else:
         print("Unknown classifier:", options.classifier)
         print(USAGE_STRING)
--- test.py
+++ test.py
@@ -5,48 +5,39 @@ import datetime
 with open('results.txt', 'w') as file:
     pass
 
-# Append values to the face and digit arrays to see how the algorithms perform with larger percentages of training data.
-# Only using 10% of each data set to make it quick.
-# You can also choose how many iterations of each to run.
-# Note: The values for 10%-100% of training data for Faces are as follows:
-# 45, 90, 135, 180, 225, 270, 315, 360, 405, 451
-# For digits:
-# 500, 1000, 1500, 2000, 2500, 3000, 3500, 4000, 4500, 5000
-face_array = [45, 90, 135, 180, 225, 270, 315, 360, 405, 451]
-digit_array = [500, 1000, 1500, 2000, 2500, 3000, 3500, 4000, 4500, 5000]
+# Values for training data with 10% increase; Use arrays to show improved accuracy with larger datasets
+faces = [45, 90, 135, 180, 225, 270, 315, 360, 405, 451]
+digits = [500, 1000, 1500, 2000, 2500, 3000, 3500, 4000, 4500, 5000]
 iterations = 1
 
-# Choose which algorithms to run
-run_naive_bayes = False
-run_perceptron = False
-run_knn = True
+# Which algorithms to run
+run_nb = True
+run_perceptron = True
+run_mira = True
 
-# Used to globally track how much time has passed
-start_time = datetime.datetime.now()
-
-# -----------------------------------------------------NAIVE BAYES-----------------------------------------------------------
-if run_naive_bayes:
-    for amount in face_array:
+# Run Naive Bayes
+if run_nb:
+    for amount in faces:
         start_faces = datetime.datetime.now()
         for i in range(iterations):
-            print(f"Running naiveBayes Faces: {amount}")
+            print(f"Running Naive Bayes Faces: {amount}")
             subprocess.call(
                 f"python dataClassifier.py -c naiveBayes -d faces -t {amount} >> results.txt", shell=True)
         end_faces = datetime.datetime.now()
         print(f"{(end_faces - start_faces).total_seconds()} seconds")
 
-    for amount in digit_array:
+    for amount in digits:
         start_digits = datetime.datetime.now()
         for i in range(iterations):
-            print(f"Running naiveBayes Digits: {amount}")
+            print(f"Running Naive Bayes Digits: {amount}")
             subprocess.call(
                 f"python dataClassifier.py -c naiveBayes -d digits -t {amount}  >> results.txt", shell=True)
         end_digits = datetime.datetime.now()
         print(f"{(end_digits - start_digits).total_seconds()} seconds")
 
-# -----------------------------------------------------PERCEPTRON-----------------------------------------------------------
+# Run Perceptron
 if run_perceptron:
-    for amount in face_array:
+    for amount in faces:
         start_faces = datetime.datetime.now()
         for i in range(iterations):
             print(f"Running Perceptron Faces: {amount}")
@@ -55,7 +46,7 @@ if run_perceptron:
         end_faces = datetime.datetime.now()
         print(f"{(end_faces - start_faces).total_seconds()} seconds")
 
-    for amount in digit_array:
+    for amount in digits:
         start_digits = datetime.datetime.now()
         for i in range(iterations):
             print(f"Running Perceptron Digits: {amount}")
@@ -64,26 +55,23 @@ if run_perceptron:
         end_digits = datetime.datetime.now()
         print(f"{(end_digits - start_digits).total_seconds()} seconds")
 
-# -------------------------------------------------k NEAREST NEIGHBOR--------------------------------------------------------
-if run_knn:
+# Run Mira
+if run_mira:
 
-    for amount in face_array:
+    for amount in faces:
         start_faces = datetime.datetime.now()
         for i in range(iterations):
-            print(f"Running KNN Faces: {amount}")
+            print(f"Running Mira Faces: {amount}")
             subprocess.call(
-                f"python dataClassifier.py -c knn -d faces -t {amount} >> results.txt", shell=True)
+                f"python dataClassifier.py -c mira -d faces -t {amount} >> results.txt", shell=True)
         end_faces = datetime.datetime.now()
-        print(f"{end_faces - start_faces} seconds")
-
-    for amount in digit_array:
-        start_digits = datetime.datetime.now()
-        for i in range(iterations):
-            print(f"Running KNN Digits: {amount}")
-            subprocess.call(
-                f"python dataClassifier.py -c kNN -d digits -t {amount} >> results.txt", shell=True)
-        end_digits = datetime.datetime.now()
         print(f"{(end_digits - start_digits).total_seconds()} seconds")
 
-end_time = datetime.datetime.now()
-print(f"{end_time - start_time} seconds (TOTAL)")
+    for amount in digits:
+        start_digits = datetime.datetime.now()
+        for i in range(iterations):
+            print(f"Running Mira Digits: {amount}")
+            subprocess.call(
+                f"python dataClassifier.py -c mira -d digits -t {amount} >> results.txt", shell=True)
+        end_digits = datetime.datetime.now()
+        print(f"{(end_digits - start_digits).total_seconds()} seconds")
